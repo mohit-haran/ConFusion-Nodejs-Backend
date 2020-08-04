80f509642b608be6367c7976c1b63779d15d9419
mohit-haran <mohitharan1999@gmail.com>
2020-08-04 11:46:11 +0530

    second-commit

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     secretKey: '12345-67890-09876-54321',
     mongoURL: 'mongodb://localhost:27017/courseranodemongo',
     facebook: {
-        clientId:'273949143717970',
-        clientSecret:'f5ece3ca3f1e8f9d0d63f30f4e35c930'
+        clientId:'fb-client-id',
+        clientSecret:'fb-client-secret'
     }
 }
